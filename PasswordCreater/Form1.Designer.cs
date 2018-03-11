eb8d4debf9fdeb37840ed417bcbacb913722c64d
lycheejam <lycheejamwow@gmail.com>
2018-03-11 23:51:15 +0900

    文字列生成部分を小分けに それと画面を作り始めた メソッド名はまた変わると思う

--- PasswordCreater/Form1.Designer.cs
+++ PasswordCreater/Form1.Designer.cs
@@ -23,34 +23,180 @@
         /// コード エディターで変更しないでください。
         /// </summary>
         private void InitializeComponent() {
-            this.button1 = new System.Windows.Forms.Button();
+            this.CreateButton = new System.Windows.Forms.Button();
+            this.Characters = new System.Windows.Forms.GroupBox();
+            this.UpeCharCB = new System.Windows.Forms.CheckBox();
+            this.LowCharCB = new System.Windows.Forms.CheckBox();
+            this.NumCharCB = new System.Windows.Forms.CheckBox();
+            this.SymCharCB = new System.Windows.Forms.CheckBox();
+            this.CharactersLen = new System.Windows.Forms.GroupBox();
+            this.CharLen8 = new System.Windows.Forms.RadioButton();
+            this.CharLen16 = new System.Windows.Forms.RadioButton();
+            this.CharLenCustom = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Passwd = new System.Windows.Forms.GroupBox();
+            this.Characters.SuspendLayout();
+            this.CharactersLen.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // CreateButton
             // 
-            this.button1.Location = new System.Drawing.Point(68, 60);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(141, 78);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.CreateButton.Location = new System.Drawing.Point(12, 12);
+            this.CreateButton.Name = "CreateButton";
+            this.CreateButton.Size = new System.Drawing.Size(141, 53);
+            this.CreateButton.TabIndex = 0;
+            this.CreateButton.Text = "パスワード生成";
+            this.CreateButton.UseVisualStyleBackColor = true;
+            this.CreateButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Characters
+            // 
+            this.Characters.Controls.Add(this.SymCharCB);
+            this.Characters.Controls.Add(this.NumCharCB);
+            this.Characters.Controls.Add(this.LowCharCB);
+            this.Characters.Controls.Add(this.UpeCharCB);
+            this.Characters.Location = new System.Drawing.Point(12, 72);
+            this.Characters.Name = "Characters";
+            this.Characters.Size = new System.Drawing.Size(141, 115);
+            this.Characters.TabIndex = 1;
+            this.Characters.TabStop = false;
+            this.Characters.Text = "使用文字";
+            // 
+            // UpeCharCB
+            // 
+            this.UpeCharCB.AutoSize = true;
+            this.UpeCharCB.Location = new System.Drawing.Point(7, 19);
+            this.UpeCharCB.Name = "UpeCharCB";
+            this.UpeCharCB.Size = new System.Drawing.Size(92, 16);
+            this.UpeCharCB.TabIndex = 0;
+            this.UpeCharCB.Text = "英字(大文字)";
+            this.UpeCharCB.UseVisualStyleBackColor = true;
+            // 
+            // LowCharCB
+            // 
+            this.LowCharCB.AutoSize = true;
+            this.LowCharCB.Location = new System.Drawing.Point(7, 42);
+            this.LowCharCB.Name = "LowCharCB";
+            this.LowCharCB.Size = new System.Drawing.Size(92, 16);
+            this.LowCharCB.TabIndex = 1;
+            this.LowCharCB.Text = "英字(小文字)";
+            this.LowCharCB.UseVisualStyleBackColor = true;
+            // 
+            // NumCharCB
+            // 
+            this.NumCharCB.AutoSize = true;
+            this.NumCharCB.Location = new System.Drawing.Point(7, 65);
+            this.NumCharCB.Name = "NumCharCB";
+            this.NumCharCB.Size = new System.Drawing.Size(48, 16);
+            this.NumCharCB.TabIndex = 2;
+            this.NumCharCB.Text = "数字";
+            this.NumCharCB.UseVisualStyleBackColor = true;
+            // 
+            // SymCharCB
+            // 
+            this.SymCharCB.AutoSize = true;
+            this.SymCharCB.Location = new System.Drawing.Point(7, 88);
+            this.SymCharCB.Name = "SymCharCB";
+            this.SymCharCB.Size = new System.Drawing.Size(48, 16);
+            this.SymCharCB.TabIndex = 3;
+            this.SymCharCB.Text = "記号";
+            this.SymCharCB.UseVisualStyleBackColor = true;
+            // 
+            // CharactersLen
+            // 
+            this.CharactersLen.Controls.Add(this.textBox1);
+            this.CharactersLen.Controls.Add(this.CharLenCustom);
+            this.CharactersLen.Controls.Add(this.CharLen16);
+            this.CharactersLen.Controls.Add(this.CharLen8);
+            this.CharactersLen.Location = new System.Drawing.Point(13, 194);
+            this.CharactersLen.Name = "CharactersLen";
+            this.CharactersLen.Size = new System.Drawing.Size(140, 100);
+            this.CharactersLen.TabIndex = 2;
+            this.CharactersLen.TabStop = false;
+            this.CharactersLen.Text = "パスワード文字数";
+            // 
+            // CharLen8
+            // 
+            this.CharLen8.AutoSize = true;
+            this.CharLen8.Location = new System.Drawing.Point(6, 19);
+            this.CharLen8.Name = "CharLen8";
+            this.CharLen8.Size = new System.Drawing.Size(53, 16);
+            this.CharLen8.TabIndex = 0;
+            this.CharLen8.TabStop = true;
+            this.CharLen8.Text = "8文字";
+            this.CharLen8.UseVisualStyleBackColor = true;
+            // 
+            // CharLen16
+            // 
+            this.CharLen16.AutoSize = true;
+            this.CharLen16.Location = new System.Drawing.Point(6, 42);
+            this.CharLen16.Name = "CharLen16";
+            this.CharLen16.Size = new System.Drawing.Size(59, 16);
+            this.CharLen16.TabIndex = 1;
+            this.CharLen16.TabStop = true;
+            this.CharLen16.Text = "16文字";
+            this.CharLen16.UseVisualStyleBackColor = true;
+            // 
+            // CharLenCustom
+            // 
+            this.CharLenCustom.AutoSize = true;
+            this.CharLenCustom.Location = new System.Drawing.Point(6, 65);
+            this.CharLenCustom.Name = "CharLenCustom";
+            this.CharLenCustom.Size = new System.Drawing.Size(47, 16);
+            this.CharLenCustom.TabIndex = 2;
+            this.CharLenCustom.TabStop = true;
+            this.CharLenCustom.Text = "任意";
+            this.CharLenCustom.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(74, 19);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Passwd
+            // 
+            this.Passwd.Location = new System.Drawing.Point(12, 301);
+            this.Passwd.Name = "Passwd";
+            this.Passwd.Size = new System.Drawing.Size(200, 100);
+            this.Passwd.TabIndex = 3;
+            this.Passwd.TabStop = false;
+            this.Passwd.Text = "groupBox1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(590, 539);
+            this.Controls.Add(this.Passwd);
+            this.Controls.Add(this.CharactersLen);
+            this.Controls.Add(this.Characters);
+            this.Controls.Add(this.CreateButton);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Characters.ResumeLayout(false);
+            this.Characters.PerformLayout();
+            this.CharactersLen.ResumeLayout(false);
+            this.CharactersLen.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button CreateButton;
+        private System.Windows.Forms.GroupBox Characters;
+        private System.Windows.Forms.CheckBox SymCharCB;
+        private System.Windows.Forms.CheckBox NumCharCB;
+        private System.Windows.Forms.CheckBox LowCharCB;
+        private System.Windows.Forms.CheckBox UpeCharCB;
+        private System.Windows.Forms.GroupBox CharactersLen;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton CharLenCustom;
+        private System.Windows.Forms.RadioButton CharLen16;
+        private System.Windows.Forms.RadioButton CharLen8;
+        private System.Windows.Forms.GroupBox Passwd;
     }
 }
 
--- PasswordCreater/Password.cs
+++ PasswordCreater/Password.cs
@@ -6,52 +6,7 @@ using System.Threading.Tasks;
 
 namespace PasswordCreater {
     class Password {
-        public string CharacterCreate() {
-            var sr = new StringBuilder();
-            Console.WriteLine("\r\n数字");
-            //0~9
-            for (int i = 0; i < 10; i++) {
-                sr.Append(i);
-                Console.Write(i);
-            }
-            Console.WriteLine("\r\n大文字");
-            //大文字
-            for (int i = 0x41; i < 0x5B; i++) {
-                sr.Append((Char)i);
-                Console.Write((Char)i);
-            }
-            Console.WriteLine("\r\n小文字");
-            //小文字
-            for (int i = 0x61; i < 0x7B; i++) {
-                sr.Append((Char)i);
-                Console.Write((Char)i);
-            }
-            Console.WriteLine("\r\n記号");
-            //記号
-            for (int i = 0x21; i < 0x2F; i++) {
-                sr.Append((Char)i);
-                Console.Write((Char)i);
-            }
-
-            return sr.ToString();
-        }
-
-        //public string CharacterCreate() {
-        //    //もはやこっちのほうが簡潔じゃね？
-        //    var numChar = "0123456789";
-        //    var upeChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        //    var lowChar = "abcdefghijklmnopqrstuvwxyz";
-        //    var symChar = "!\"#$%&'()*+,-.";
-
-        //    var sr = new StringBuilder();
-        //    sr.Append(numChar);
-        //    sr.Append(upeChar);
-        //    sr.Append(lowChar);
-        //    sr.Append(symChar);
-
-        //    return sr.ToString();
-        //}
-        
+        //パスワード生成部分
         public string PasswdCreate(int pwdLen, string pwdChar) {
             var sr = new StringBuilder(pwdLen);
 
@@ -61,6 +16,58 @@ namespace PasswordCreater {
                 sr.Append(pwdChar[rnd.Next(pwdChar.Length)]);
             }
 
+            return sr.ToString();
+        }
+        /// <summary>
+        /// 大文字作成
+        /// </summary>
+        /// <param name="s">既存文字列</param>
+        /// <returns>既存文字列+大文字</returns>
+        public string UpeCharCreate(string s) {
+            var sr = new StringBuilder();
+            sr.Append(s);
+            for (int i = 0x41; i < 0x5B; i++) {
+                sr.Append((Char)i);
+            }
+            return sr.ToString();
+        }
+        /// <summary>
+        /// 小文字作成
+        /// </summary>
+        /// <param name="s">既存文字列</param>
+        /// <returns>既存文字列+小文字</returns>
+        public string LowCharCreate(string s) {
+            var sr = new StringBuilder();
+            sr.Append(s);
+            for (int i = 0x61; i < 0x7B; i++) {
+                sr.Append((Char)i);
+            }
+            return sr.ToString();
+        }
+        /// <summary>
+        /// 数字作成
+        /// </summary>
+        /// <param name="s">既存文字列</param>
+        /// <returns>既存文字列+数字</returns>
+        public string NumCharCreate(string s) {
+            var sr = new StringBuilder();
+            sr.Append(s);
+            for (int i = 0; i < 10; i++) {
+                sr.Append((Char)i);
+            }
+            return sr.ToString();
+        }
+        /// <summary>
+        /// 記号作成
+        /// </summary>
+        /// <param name="s">既存文字列</param>
+        /// <returns>既存文字列+記号</returns>
+        public string SymCharCreate(string s) {
+            var sr = new StringBuilder();
+            sr.Append(s);
+            for (int i = 0x21; i < 0x2F; i++) {
+                sr.Append((Char)i);
+            }
             return sr.ToString();
         }
     }
