4980745927604e2e6f696bdbe3147cc0ef13047f
lycheejam <lycheejamwow@gmail.com>
2018-03-12 22:26:36 +0900

    ラジオボタンのイベントを作成 任意を選択時にテキストボックスを有効化

--- PasswordCreater/Form1.Designer.cs
+++ PasswordCreater/Form1.Designer.cs
@@ -25,23 +25,25 @@
         private void InitializeComponent() {
             this.CreateButton = new System.Windows.Forms.Button();
             this.Characters = new System.Windows.Forms.GroupBox();
-            this.UpeCharCB = new System.Windows.Forms.CheckBox();
-            this.LowCharCB = new System.Windows.Forms.CheckBox();
-            this.NumCharCB = new System.Windows.Forms.CheckBox();
             this.SymCharCB = new System.Windows.Forms.CheckBox();
+            this.NumCharCB = new System.Windows.Forms.CheckBox();
+            this.LowCharCB = new System.Windows.Forms.CheckBox();
+            this.UpeCharCB = new System.Windows.Forms.CheckBox();
             this.CharactersLen = new System.Windows.Forms.GroupBox();
-            this.CharLen8 = new System.Windows.Forms.RadioButton();
-            this.CharLen16 = new System.Windows.Forms.RadioButton();
-            this.CharLenCustom = new System.Windows.Forms.RadioButton();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.GeneratePasswdCnt = new System.Windows.Forms.GroupBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.CharLenTbCustom = new System.Windows.Forms.TextBox();
+            this.CharLenRbCustom = new System.Windows.Forms.RadioButton();
+            this.CharLenRb16 = new System.Windows.Forms.RadioButton();
+            this.CharLenRb8 = new System.Windows.Forms.RadioButton();
+            this.CrePassCustom = new System.Windows.Forms.GroupBox();
+            this.CrePassTbCustom = new System.Windows.Forms.TextBox();
+            this.CrePassRbCustom = new System.Windows.Forms.RadioButton();
+            this.CrePassRb20 = new System.Windows.Forms.RadioButton();
+            this.CrePassRb10 = new System.Windows.Forms.RadioButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Characters.SuspendLayout();
             this.CharactersLen.SuspendLayout();
-            this.GeneratePasswdCnt.SuspendLayout();
+            this.CrePassCustom.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // CreateButton
@@ -67,25 +69,15 @@
             this.Characters.TabStop = false;
             this.Characters.Text = "使用文字";
             // 
-            // UpeCharCB
+            // SymCharCB
             // 
-            this.UpeCharCB.AutoSize = true;
-            this.UpeCharCB.Location = new System.Drawing.Point(7, 19);
-            this.UpeCharCB.Name = "UpeCharCB";
-            this.UpeCharCB.Size = new System.Drawing.Size(92, 16);
-            this.UpeCharCB.TabIndex = 0;
-            this.UpeCharCB.Text = "英字(大文字)";
-            this.UpeCharCB.UseVisualStyleBackColor = true;
-            // 
-            // LowCharCB
-            // 
-            this.LowCharCB.AutoSize = true;
-            this.LowCharCB.Location = new System.Drawing.Point(7, 42);
-            this.LowCharCB.Name = "LowCharCB";
-            this.LowCharCB.Size = new System.Drawing.Size(92, 16);
-            this.LowCharCB.TabIndex = 1;
-            this.LowCharCB.Text = "英字(小文字)";
-            this.LowCharCB.UseVisualStyleBackColor = true;
+            this.SymCharCB.AutoSize = true;
+            this.SymCharCB.Location = new System.Drawing.Point(7, 88);
+            this.SymCharCB.Name = "SymCharCB";
+            this.SymCharCB.Size = new System.Drawing.Size(48, 16);
+            this.SymCharCB.TabIndex = 3;
+            this.SymCharCB.Text = "記号";
+            this.SymCharCB.UseVisualStyleBackColor = true;
             // 
             // NumCharCB
             // 
@@ -97,22 +89,32 @@
             this.NumCharCB.Text = "数字";
             this.NumCharCB.UseVisualStyleBackColor = true;
             // 
-            // SymCharCB
+            // LowCharCB
             // 
-            this.SymCharCB.AutoSize = true;
-            this.SymCharCB.Location = new System.Drawing.Point(7, 88);
-            this.SymCharCB.Name = "SymCharCB";
-            this.SymCharCB.Size = new System.Drawing.Size(48, 16);
-            this.SymCharCB.TabIndex = 3;
-            this.SymCharCB.Text = "記号";
-            this.SymCharCB.UseVisualStyleBackColor = true;
+            this.LowCharCB.AutoSize = true;
+            this.LowCharCB.Location = new System.Drawing.Point(7, 42);
+            this.LowCharCB.Name = "LowCharCB";
+            this.LowCharCB.Size = new System.Drawing.Size(92, 16);
+            this.LowCharCB.TabIndex = 1;
+            this.LowCharCB.Text = "英字(小文字)";
+            this.LowCharCB.UseVisualStyleBackColor = true;
+            // 
+            // UpeCharCB
+            // 
+            this.UpeCharCB.AutoSize = true;
+            this.UpeCharCB.Location = new System.Drawing.Point(7, 19);
+            this.UpeCharCB.Name = "UpeCharCB";
+            this.UpeCharCB.Size = new System.Drawing.Size(92, 16);
+            this.UpeCharCB.TabIndex = 0;
+            this.UpeCharCB.Text = "英字(大文字)";
+            this.UpeCharCB.UseVisualStyleBackColor = true;
             // 
             // CharactersLen
             // 
-            this.CharactersLen.Controls.Add(this.textBox1);
-            this.CharactersLen.Controls.Add(this.CharLenCustom);
-            this.CharactersLen.Controls.Add(this.CharLen16);
-            this.CharactersLen.Controls.Add(this.CharLen8);
+            this.CharactersLen.Controls.Add(this.CharLenTbCustom);
+            this.CharactersLen.Controls.Add(this.CharLenRbCustom);
+            this.CharactersLen.Controls.Add(this.CharLenRb16);
+            this.CharactersLen.Controls.Add(this.CharLenRb8);
             this.CharactersLen.Location = new System.Drawing.Point(13, 194);
             this.CharactersLen.Name = "CharactersLen";
             this.CharactersLen.Size = new System.Drawing.Size(140, 92);
@@ -120,105 +122,121 @@
             this.CharactersLen.TabStop = false;
             this.CharactersLen.Text = "パスワード文字数";
             // 
-            // CharLen8
+            // CharLenTbCustom
             // 
-            this.CharLen8.AutoSize = true;
-            this.CharLen8.Location = new System.Drawing.Point(6, 19);
-            this.CharLen8.Name = "CharLen8";
-            this.CharLen8.Size = new System.Drawing.Size(53, 16);
-            this.CharLen8.TabIndex = 0;
-            this.CharLen8.TabStop = true;
-            this.CharLen8.Text = "8文字";
-            this.CharLen8.UseVisualStyleBackColor = true;
+            this.CharLenTbCustom.Enabled = false;
+            this.CharLenTbCustom.Location = new System.Drawing.Point(59, 64);
+            this.CharLenTbCustom.Name = "CharLenTbCustom";
+            this.CharLenTbCustom.Size = new System.Drawing.Size(74, 19);
+            this.CharLenTbCustom.TabIndex = 3;
             // 
-            // CharLen16
+            // CharLenRbCustom
             // 
-            this.CharLen16.AutoSize = true;
-            this.CharLen16.Location = new System.Drawing.Point(6, 42);
-            this.CharLen16.Name = "CharLen16";
-            this.CharLen16.Size = new System.Drawing.Size(59, 16);
-            this.CharLen16.TabIndex = 1;
-            this.CharLen16.TabStop = true;
-            this.CharLen16.Text = "16文字";
-            this.CharLen16.UseVisualStyleBackColor = true;
+            this.CharLenRbCustom.AutoSize = true;
+            this.CharLenRbCustom.Location = new System.Drawing.Point(6, 65);
+            this.CharLenRbCustom.Name = "CharLenRbCustom";
+            this.CharLenRbCustom.Size = new System.Drawing.Size(47, 16);
+            this.CharLenRbCustom.TabIndex = 2;
+            this.CharLenRbCustom.Text = "任意";
+            this.CharLenRbCustom.UseVisualStyleBackColor = true;
+            this.CharLenRbCustom.CheckedChanged += new System.EventHandler(this.CharLenRb_CheckedChanged);
             // 
-            // CharLenCustom
+            // CharLenRb16
             // 
-            this.CharLenCustom.AutoSize = true;
-            this.CharLenCustom.Location = new System.Drawing.Point(6, 65);
-            this.CharLenCustom.Name = "CharLenCustom";
-            this.CharLenCustom.Size = new System.Drawing.Size(47, 16);
-            this.CharLenCustom.TabIndex = 2;
-            this.CharLenCustom.TabStop = true;
-            this.CharLenCustom.Text = "任意";
-            this.CharLenCustom.UseVisualStyleBackColor = true;
+            this.CharLenRb16.AutoSize = true;
+            this.CharLenRb16.Location = new System.Drawing.Point(6, 42);
+            this.CharLenRb16.Name = "CharLenRb16";
+            this.CharLenRb16.Size = new System.Drawing.Size(59, 16);
+            this.CharLenRb16.TabIndex = 1;
+            this.CharLenRb16.Text = "16文字";
+            this.CharLenRb16.UseVisualStyleBackColor = true;
+            this.CharLenRb16.CheckedChanged += new System.EventHandler(this.CharLenRb_CheckedChanged);
             // 
-            // textBox1
+            // CharLenRb8
             // 
-            this.textBox1.Location = new System.Drawing.Point(59, 64);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(74, 19);
-            this.textBox1.TabIndex = 3;
+            this.CharLenRb8.AutoSize = true;
+            this.CharLenRb8.Checked = true;
+            this.CharLenRb8.Location = new System.Drawing.Point(6, 19);
+            this.CharLenRb8.Name = "CharLenRb8";
+            this.CharLenRb8.Size = new System.Drawing.Size(53, 16);
+            this.CharLenRb8.TabIndex = 0;
+            this.CharLenRb8.TabStop = true;
+            this.CharLenRb8.Text = "8文字";
+            this.CharLenRb8.UseVisualStyleBackColor = true;
+            this.CharLenRb8.CheckedChanged += new System.EventHandler(this.CharLenRb_CheckedChanged);
             // 
-            // GeneratePasswdCnt
+            // CrePassCustom
             // 
-            this.GeneratePasswdCnt.Controls.Add(this.textBox2);
-            this.GeneratePasswdCnt.Controls.Add(this.radioButton3);
-            this.GeneratePasswdCnt.Controls.Add(this.radioButton2);
-            this.GeneratePasswdCnt.Controls.Add(this.radioButton1);
-            this.GeneratePasswdCnt.Location = new System.Drawing.Point(12, 292);
-            this.GeneratePasswdCnt.Name = "GeneratePasswdCnt";
-            this.GeneratePasswdCnt.Size = new System.Drawing.Size(141, 92);
-            this.GeneratePasswdCnt.TabIndex = 3;
-            this.GeneratePasswdCnt.TabStop = false;
-            this.GeneratePasswdCnt.Text = "パスワード生成数";
+            this.CrePassCustom.Controls.Add(this.CrePassTbCustom);
+            this.CrePassCustom.Controls.Add(this.CrePassRbCustom);
+            this.CrePassCustom.Controls.Add(this.CrePassRb20);
+            this.CrePassCustom.Controls.Add(this.CrePassRb10);
+            this.CrePassCustom.Location = new System.Drawing.Point(12, 292);
+            this.CrePassCustom.Name = "CrePassCustom";
+            this.CrePassCustom.Size = new System.Drawing.Size(141, 92);
+            this.CrePassCustom.TabIndex = 3;
+            this.CrePassCustom.TabStop = false;
+            this.CrePassCustom.Text = "パスワード生成数";
             // 
-            // radioButton1
+            // CrePassTbCustom
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(7, 19);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(35, 16);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "10";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.CrePassTbCustom.Enabled = false;
+            this.CrePassTbCustom.Location = new System.Drawing.Point(60, 64);
+            this.CrePassTbCustom.Name = "CrePassTbCustom";
+            this.CrePassTbCustom.Size = new System.Drawing.Size(73, 19);
+            this.CrePassTbCustom.TabIndex = 3;
             // 
-            // radioButton2
+            // CrePassRbCustom
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(7, 42);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(35, 16);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "20";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.CrePassRbCustom.AutoSize = true;
+            this.CrePassRbCustom.Location = new System.Drawing.Point(7, 65);
+            this.CrePassRbCustom.Name = "CrePassRbCustom";
+            this.CrePassRbCustom.Size = new System.Drawing.Size(47, 16);
+            this.CrePassRbCustom.TabIndex = 2;
+            this.CrePassRbCustom.Text = "任意";
+            this.CrePassRbCustom.UseVisualStyleBackColor = true;
+            this.CrePassRbCustom.CheckedChanged += new System.EventHandler(this.CrePassRb_CheckedChanged);
             // 
-            // radioButton3
+            // CrePassRb20
             // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(7, 65);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(47, 16);
-            this.radioButton3.TabIndex = 2;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "任意";
-            this.radioButton3.UseVisualStyleBackColor = true;
+            this.CrePassRb20.AutoSize = true;
+            this.CrePassRb20.Location = new System.Drawing.Point(7, 42);
+            this.CrePassRb20.Name = "CrePassRb20";
+            this.CrePassRb20.Size = new System.Drawing.Size(35, 16);
+            this.CrePassRb20.TabIndex = 1;
+            this.CrePassRb20.Text = "20";
+            this.CrePassRb20.UseVisualStyleBackColor = true;
+            this.CrePassRb20.CheckedChanged += new System.EventHandler(this.CrePassRb_CheckedChanged);
             // 
-            // textBox2
+            // CrePassRb10
             // 
-            this.textBox2.Location = new System.Drawing.Point(60, 64);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(73, 19);
-            this.textBox2.TabIndex = 3;
+            this.CrePassRb10.AutoSize = true;
+            this.CrePassRb10.Checked = true;
+            this.CrePassRb10.Location = new System.Drawing.Point(7, 19);
+            this.CrePassRb10.Name = "CrePassRb10";
+            this.CrePassRb10.Size = new System.Drawing.Size(35, 16);
+            this.CrePassRb10.TabIndex = 0;
+            this.CrePassRb10.TabStop = true;
+            this.CrePassRb10.Text = "10";
+            this.CrePassRb10.UseVisualStyleBackColor = true;
+            this.CrePassRb10.CheckedChanged += new System.EventHandler(this.CrePassRb_CheckedChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(159, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(419, 372);
+            this.dataGridView1.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(590, 390);
-            this.Controls.Add(this.GeneratePasswdCnt);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.CrePassCustom);
             this.Controls.Add(this.CharactersLen);
             this.Controls.Add(this.Characters);
             this.Controls.Add(this.CreateButton);
@@ -228,8 +246,9 @@
             this.Characters.PerformLayout();
             this.CharactersLen.ResumeLayout(false);
             this.CharactersLen.PerformLayout();
-            this.GeneratePasswdCnt.ResumeLayout(false);
-            this.GeneratePasswdCnt.PerformLayout();
+            this.CrePassCustom.ResumeLayout(false);
+            this.CrePassCustom.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -243,15 +262,16 @@
         private System.Windows.Forms.CheckBox LowCharCB;
         private System.Windows.Forms.CheckBox UpeCharCB;
         private System.Windows.Forms.GroupBox CharactersLen;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.RadioButton CharLenCustom;
-        private System.Windows.Forms.RadioButton CharLen16;
-        private System.Windows.Forms.RadioButton CharLen8;
-        private System.Windows.Forms.GroupBox GeneratePasswdCnt;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox CharLenTbCustom;
+        private System.Windows.Forms.RadioButton CharLenRbCustom;
+        private System.Windows.Forms.RadioButton CharLenRb16;
+        private System.Windows.Forms.RadioButton CharLenRb8;
+        private System.Windows.Forms.GroupBox CrePassCustom;
+        private System.Windows.Forms.TextBox CrePassTbCustom;
+        private System.Windows.Forms.RadioButton CrePassRbCustom;
+        private System.Windows.Forms.RadioButton CrePassRb20;
+        private System.Windows.Forms.RadioButton CrePassRb10;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- PasswordCreater/Form1.cs
+++ PasswordCreater/Form1.cs
@@ -15,10 +15,30 @@ namespace PasswordCreater {
         }
 
         private void button1_Click(object sender, EventArgs e) {
-            Password pw = new Password();
-            var s = pw.CharacterCreate();
-            Console.WriteLine("\r\n結果");
-            Console.Write(pw.PasswdCreate(8, s));
+            //Password pw = new Password();
+            //var s = pw.CharacterCreate();
+            //Console.WriteLine("\r\n結果");
+            //Console.Write(pw.PasswdCreate(8, s));
+        }
+
+        private void CrePassRb_CheckedChanged(object sender, EventArgs e) {
+            if (CrePassRbCustom.Checked == true) {
+                //テキストボックスを使用可能に変更
+                CrePassTbCustom.Enabled = true;
+            } else if (CrePassRb10.Checked == true || 
+                       CrePassRb20.Checked == true) {
+                //テキストボックスを使用不可に変更
+                CrePassTbCustom.Enabled = false;
+            }
+        }
+
+        private void CharLenRb_CheckedChanged(object sender, EventArgs e) {
+            if (CharLenRbCustom.Checked == true) {
+                CharLenTbCustom.Enabled = true;
+            } else if (CharLenRb8.Checked == true ||
+                       CharLenRb16.Checked == true) {
+                CharLenTbCustom.Enabled = false;
+            }
         }
     }
 }
